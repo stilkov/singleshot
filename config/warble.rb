fd5c960b6a7bb3856489f6e95b11c7110aa71a63
jmuzz <jmuzz@ftw.(none)>
2009-08-14 15:09:50 -0700

    Added some required gems to the warble config

--- config/warble.rb
+++ config/warble.rb
@@ -35,6 +35,7 @@ Warbler::Config.new do |config|
   # The Rails gems are included by default unless the vendor/rails directory is present.
   # config.gems += ["activerecord-jdbcmysql-adapter", "jruby-openssl"]
   # config.gems << "tzinfo"
+  config.gems += ["jruby-openssl", "activerecord-jdbc-adapter", "activerecord-jdbcmysql-adapter"]
 
   # Uncomment this if you don't want to package rails gem.
   # config.gems -= ["rails"]
